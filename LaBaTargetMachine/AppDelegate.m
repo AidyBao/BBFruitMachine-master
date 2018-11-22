3bd77f259a6d2643cf1680f47455ed3050cc03a5
AidyBao <aidybao555@sina.com>
2018-11-22 09:30:47 +0800

    1.0.0
    水果机

---
+++ LaBaTargetMachine/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  LaBaTargetMachine
+//
+//  Created by 腾程－ios1 on 16/10/17.
+//  Copyright © 2016年 腾程－zsw. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import <UIKit/UIKit.h>
+#import "DemoViewController.h"
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    return YES;
+}
+
+
+@end
